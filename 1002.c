5d1dc55ee96cb05bbd40bd4c6204de9e0d3d4356
tangxiangru <871607149@qq.com>
2017-05-08 23:54:34 +0800

    17-5-8

--- 1002.c
+++ 1002.c
@@ -1,4 +1,8 @@
-/*#include <stdio.h>
+/*
+输入一个整数 n 。
+计算 n 的每一位的相加的和 sum
+以拼音的方式顺位输出
+#include <stdio.h>
 int main()
 {
   ing n,sum,num,weishu;
@@ -13,7 +17,8 @@ int main()
     sum=num-sum%10;
   }
 }
-走弯路了*/
+走弯路了,因为n范围很大.. 小于 $10^{100}，不能用int
+定义一个100位的字符数组存储输入的“数据”，然后for循环各个位置相加，得到和。先确定和的长度，再挨个输出来，我这里定义了一个字符指针数组，根据每位的值直接调取即可。程序里是采用 先‘%’ 后‘/’，所以要倒过来输出。*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
